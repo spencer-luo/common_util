4f65457412da5dc4a2ca97f103586c57fec59507
spencer <spencer.luo@foxmail.com>
2024-04-17 22:46:09 +0800

    feat: add TestRemoveFileAndDir

--- src/usage_demo/fileutil.hpp
+++ src/usage_demo/fileutil.hpp
@@ -3,6 +3,8 @@
 #include "common.hpp"
 #include "common_util/fileutil.h"
 
+static const std::string kBaseDir = "/Users/spencer/workspace/common_util/fileutil_test";
+
 void TestGetCwd()
 {
     PrintSubTitle("getcwd");
@@ -15,19 +17,38 @@ void TestCreateFileAndDir()
 {
     PrintSubTitle("create file/dir");
 
-    auto basedir = cutl::path("/Users/spencer/workspace/common_util/fileutil_test");
+    auto basedir = cutl::path(kBaseDir);
     cutl::createdir(basedir);
     // ./dir1
     cutl::createdir(basedir.join("dir1"));
-    // ./file1.txt
-    cutl::createfile(basedir.join("file1.txt"));
-    // ./file2.txt
-    cutl::createfile(basedir.join("file2.txt"));
+    // ./file3.txt
+    cutl::createfile(basedir.join("file3.txt"));
+    // ./file4.txt
+    cutl::createfile(basedir.join("file4.txt"));
     // ./dir2/dir1
     auto dir21 = basedir.join("dir2/dir1");
     cutl::createdir(dir21, true);
     // ./dir2/dir1/file211.txt
     cutl::createfile(dir21.join("file211.txt"));
+    // ./dir2/dir1/file212.txt
+    cutl::createfile(dir21.join("file212.txt"));
+    // ./dir2/file22.data
+    auto file22 = basedir.join("dir2/file22.data");
+    cutl::createfile(file22);
+}
+
+void TestRemoveFileAndDir()
+{
+    PrintSubTitle("remove file/dir");
+
+    auto basedir = cutl::path(kBaseDir);
+
+    // ./dir1
+    cutl::removedir(basedir.join("dir1"));
+    // ./file1.txt
+    cutl::removefile(basedir.join("file3.txt"));
+    // ./dir2
+    cutl::removedir(basedir.join("dir2"), true);
 }
 
 void TestFileUtil()
@@ -35,5 +56,6 @@ void TestFileUtil()
     PrintTitle("fileutil");
 
     TestGetCwd();
-    TestCreateFileAndDir();
+    // TestCreateFileAndDir();
+    TestRemoveFileAndDir();
 }
