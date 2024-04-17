a7a8206d241c9d448b615c5278fd08d18165d2cd
spencer <spencer.luo@foxmail.com>
2024-04-17 22:30:51 +0800

    refactor TestCreateFileAndDir

--- src/usage_demo/fileutil.hpp
+++ src/usage_demo/fileutil.hpp
@@ -15,30 +15,19 @@ void TestCreateFileAndDir()
 {
     PrintSubTitle("create file/dir");
 
-    auto basepath = cutl::path("/Users/spencer/workspace/common_util/dest");
-    auto testdir = basepath.join("test_dir");
-    std::cout << "testdir: " << testdir << std::endl;
-    if (!cutl::createdir(testdir))
-    {
-        std::cout << "failed to create test_dir" << testdir << std::endl;
-        return;
-    }
-    auto testfile = testdir.join("test_file.txt");
-
-    if (!cutl::createfile(testfile))
-    {
-        std::cout << "failed to create test_file.txt" << testfile << std::endl;
-        return;
-    }
-
-    auto deepdir = basepath.join("test_dir2/test_dir3");
-    if (!cutl::createdir(deepdir, true))
-    {
-        std::cout << "failed to create deep dir" << deepdir << std::endl;
-        return;
-    }
-    auto testfile2 = deepdir.join("test_file2.txt");
-    cutl::createfile(testfile2);
+    auto basedir = cutl::path("/Users/spencer/workspace/common_util/fileutil_test");
+    cutl::createdir(basedir);
+    // ./dir1
+    cutl::createdir(basedir.join("dir1"));
+    // ./file1.txt
+    cutl::createfile(basedir.join("file1.txt"));
+    // ./file2.txt
+    cutl::createfile(basedir.join("file2.txt"));
+    // ./dir2/dir1
+    auto dir21 = basedir.join("dir2/dir1");
+    cutl::createdir(dir21, true);
+    // ./dir2/dir1/file211.txt
+    cutl::createfile(dir21.join("file211.txt"));
 }
 
 void TestFileUtil()
