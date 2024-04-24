11611142fa6b1cfb6b1045222bd35e140b59db3b
spencer.luo <spencer.luo@foxmail.com>
2024-04-24 23:19:36 +0800

    feat: add comments for timecount.h

--- src/common_util/timecount.h
+++ src/common_util/timecount.h
@@ -1,3 +1,10 @@
+/**
+ * @file timecount.h
+ * @brief A simple time counter class to measure the execution time of a function.
+ * Measure the execution time of a function by using the feature that a class instance object automatically destructs when it exits it's scope.
+ * @author spencer.luo
+ * @date 2024-04-24
+ */
 #pragma once
 
 #include <cstdint>
@@ -6,14 +13,28 @@
 
 namespace cutl
 {
+    /**
+     * @brief A simple time counter class to measure the execution time of a function.
+     *
+     */
     class timecount
     {
     public:
+        /**
+         * @brief Construct a new timecount object
+         *
+         * @param func_name
+         */
         timecount(const std::string &func_name);
+        /**
+         * @brief Destroy the timecount object
+         *
+         */
         ~timecount();
 
     private:
         std::string func_name_;
         std::atomic<uint64_t> start_time_;
     };
+
 } // namespace
