dc3ed42ee5be8d71a23d843d8fcaffc5ac08cea0
spencer.luo <spencer.luo@foxmail.com>
2024-05-03 23:50:56 +0800

    feat: add get_file_type/list_sub_files

--- src/common_util/inner/filesystem_win.cpp
+++ src/common_util/inner/filesystem_win.cpp
@@ -1,6 +1,8 @@
 #if defined(_WIN32) || defined(__WIN32__)
 
 #include <io.h>
+#include <Windows.h>
+#include "strutil.h"
 #include "filesystem.h"
 #include "logger.h"
 
@@ -78,17 +80,113 @@ namespace cutl
         return 0;
     }
 
+    filetype get_file_type(DWORD attributes, const std::string &extension)
+    {
+        filetype type = filetype::unknown;
+
+        if (attributes == INVALID_FILE_ATTRIBUTES)
+        {
+            CUTL_WARN("Failed to get file attributes, error code: " + std::to_string(GetLastError()));
+            if (extension == ".lnk")
+            {
+                // 注意：测试时发现，有些快捷方式访问会失败，用后缀名判断进行兜底
+                type = filetype::symlink;
+            }
+            return type;
+        }
+        else
+        {
+            if (attributes & FILE_ATTRIBUTE_DIRECTORY)
+            {
+                type = filetype::directory; // directory
+            }
+            else if (attributes & FILE_ATTRIBUTE_NORMAL)
+            {
+                type = filetype::file; // regular file
+            }
+            else if ((attributes & FILE_ATTRIBUTE_ARCHIVE) && extension == ".lnk")
+            {
+                // windows的快捷方式
+                type = filetype::symlink; // symbolic link
+            }
+        }
+
+        return type;
+    }
+
+    std::string get_file_extension(const std::string &filepath)
+    {
+        auto pos = filepath.find_last_of('.');
+        std::string extension = "";
+        if (pos != std::string::npos)
+        {
+            extension = filepath.substr(pos);
+            extension = cutl::to_lower(extension);
+        }
+        return extension;
+    }
+
     filetype get_file_type(const std::string &filepath)
     {
-        // todo: implement this
-        return filetype::unknown;
+        auto attributes = GetFileAttributesA(filepath.c_str());
+        auto extension = get_file_extension(filepath);
+        // CUTL_DEBUG(filepath + ", extension: " + extension + ", attributes: " + std::to_string(attributes));
+        return get_file_type(attributes, extension);
+    }
+
+    bool is_special_dir(const std::string &filePath)
+    {
+        return filePath == "." || filePath == "..";
     }
 
     filevec list_sub_files(const std::string &dirpath, filetype type, bool recursive)
     {
-        // todo: implement this
-        filevec filelist;
-        return filelist;
+        filevec file_list;
+
+        // 使用FindFirstFileA查找第一个文件
+        auto findpath = dirpath + win_separator + "*.*";
+        WIN32_FIND_DATAA findData = {0};
+        HANDLE hFind = FindFirstFileA(findpath.c_str(), &findData);
+        if (hFind == NULL || hFind == INVALID_HANDLE_VALUE)
+        {
+            CUTL_ERROR("FindFirstFileA failed for " + findpath + ", errCode: " + std::to_string(GetLastError()));
+            return file_list;
+        }
+
+        do
+        {
+            auto dwAttrs = findData.dwFileAttributes;
+            auto filename = std::string(findData.cFileName);
+            CUTL_DEBUG(filename + ", attributes: " + std::to_string(dwAttrs));
+            // “..”和“.”不做处理
+            if (is_special_dir(filename))
+            {
+                continue;
+            }
+            std::string filepath = dirpath + win_separator + filename;
+            auto extension = get_file_extension(filename);
+            auto ftype = get_file_type(dwAttrs, extension);
+            if (ftype & type)
+            {
+                file_entity entity;
+                entity.type = ftype;
+                entity.filepath = filepath;
+                file_list.emplace_back(entity);
+            }
+            if ((dwAttrs & FILE_ATTRIBUTE_DIRECTORY) && recursive)
+            {
+                // directory
+                auto sub_files = list_sub_files(filepath, type, recursive);
+                if (!sub_files.empty())
+                {
+                    file_list.insert(file_list.end(), sub_files.begin(), sub_files.end());
+                }
+            }
+        } while (FindNextFileA(hFind, &findData));
+        // 关闭句柄
+        FindClose(hFind);
+
+        return file_list;
     }
 
     bool copy_attributes(const std::string &srcpath, const std::string &dstpath)
