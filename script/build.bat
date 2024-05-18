bc8542a33cb234567482ae68295ca970c961072f
spencer.luo <spencer.luo@foxmail.com>
2024-05-18 10:16:52 +0800

    fix: build.bat

--- script/build.bat
+++ script/build.bat
@@ -12,7 +12,7 @@ if %param_count% lss 1 (
     echo .\build.bat run
     echo .\build.bat clean
     echo .\build.bat pack
-    exit
+    exit 0
 )
 
 @REM 读取可执行程序的路径
@@ -25,22 +25,23 @@ if %~1==run (
         @REM echo executable_filepath: %executable_filepath%
         %executable_filepath%
     )
-    exit
+    exit 0
 )
 
 if %~1==clean (
     @REM 参数说明：--%，停止解析符号，告诉PowerShell停止解析其余参数，并按原样传递它们
     @REM 参考文档：https://www.cnblogs.com/fireblackman/p/17323631.html
     cmd --% /c RMDIR /Q/S build
-    echo finish clean
-    exit
+    echo ./build Cleaned
+    exit 0
 )
 
 if %~1==pack (
-    @REM echo pack
+    echo Pack package...
     cd build
     cpack
-    exit
+    echo pack package Done.
+    exit 0
 )
 
 
@@ -54,18 +55,18 @@ if %~1==build (
     echo .\build.bat run
     echo .\build.bat clean
     echo .\build.bat pack
-    exit
+    exit 0
 )
 if %~1==build (
     @REM verify platform0 and buildType0
     @REM echo platform0: %platform0%
     if "%platform0%"=="" (
-        echo "platform is empty, usages:.\build.bat build [platform] [buildType]"
-        exit
+        echo "platform is empty, usages:.\build.bat build [x86|x64] [release|rls|debug|dbg]"
+        exit 0
     )
     if "%buildType0%"=="" (
-        echo "buildType is empty, usages:.\build.bat build [platform] [buildType]"
-        exit
+        echo "buildType is empty, usages:.\build.bat build [x86|x64] [release|rls|debug|dbg]"
+        exit 0
     )
 
     @REM echo build0 %platform0% %buildType0%
@@ -81,23 +82,24 @@ if %~1==build (
     if /i %platform0% == X64 (
         set platform=x64
     ) else (
-        set platform=x86
+        set platform=win32
     )
 )
 
 @REM TODO: 指定cmake的生成器，用户可以替换成自己安装的对应Visual Studio的版本
 set generator="Visual Studio 14 2015"
+@REM TODO: 指定CPU核数，用户可以根据自己电脑的CPU核数来调整设置这个参数。使用的cpu核数越多，编译速度越快。
+set cpu_cores=6
 
 if %~1==build (
     @REM echo build %platform% %buildType%
     @REM echo generator: %generator%
 
+    echo Building...
     @REM configure
     cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=%buildType% -S ./ -B ./build -G %generator% -A %platform%
     @REM build
-    @REM -j 6: 采用6个CPU核来进行并行编译，加快构建速度。
-    @REM TODO: 你可以根据自己电脑的CPU核数来调整设置这个参数
-    cmake --build ./build --config %buildType% --target ALL_BUILD -j 6 --
+    cmake --build ./build --config %buildType% --target ALL_BUILD -j %cpu_cores% --
 )
 
 
@@ -114,9 +116,3 @@ if %~1==build (
 @REM "C:\Program Files\CMake\bin\cmake.EXE" --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -SY:/common_util -By:/common_util/build -G "Visual Studio 14 2015" -A win32
 @REM build
 @REM "C:\Program Files\CMake\bin\cmake.EXE" --build y:/common_util/build --config Release --target ALL_BUILD -j 6 --
-
-@REM debug x86
-@REM configure
-@REM "C:\Program Files\CMake\bin\cmake.EXE" --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -SY:/common_util -By:/common_util/build -G "Visual Studio 14 2015" -A win32
-@REM build
-@REM "C:\Program Files\CMake\bin\cmake.EXE" --build y:/common_util/build --config Debug --target ALL_BUILD -j 6 --
