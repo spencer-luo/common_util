54bad8f810c0f478e155ec700681f58d6c83bea4
spencer.luo <spencer.luo@foxmail.com>
2024-06-15 12:50:34 +0800

    optimize tag command

--- script/publish.sh
+++ script/publish.sh
@@ -7,8 +7,9 @@
 ./build.sh release
 
 # 打标签
-git tag v1.0.0
-git push origin v1.0.0
+git tag v1.0.1
+git push origin v1.0.1
+git push github v1.0.1
 
 # 参考文档
 # https://blog.csdn.net/qq_39866016/article/details/108773144
